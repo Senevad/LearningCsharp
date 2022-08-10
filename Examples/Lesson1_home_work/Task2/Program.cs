b598efda2cc8abc515ad00decaee2c44bcb763f1
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-10 21:56:50 +0300

    Выполнил 2 задание

--- Examples/Lesson1_home_work/Task2/Program.cs
+++ Examples/Lesson1_home_work/Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 >= number2)
+{
+    Console.WriteLine("первое число '" +number1 +"' больше");
+    Console.WriteLine("второе число '" +number2 +"' меньше");
+} 
+else
+{   
+    Console.WriteLine("второе число '" +number2 +"' больше");
+    Console.WriteLine("первое число '" +number1 +"' меньше");
+}
