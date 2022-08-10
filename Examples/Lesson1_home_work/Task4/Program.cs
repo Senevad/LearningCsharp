88278978d70da5f599ba9d7e1e501cc1bae71365
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-10 22:12:12 +0300

    Выполнил 4 задание

--- Examples/Lesson1_home_work/Task4/Program.cs
+++ Examples/Lesson1_home_work/Task4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 >= number2)
+{
+    if (number1 >= number3)
+    {
+        Console.WriteLine("число '" +number1 +"' максимальное");
+    }
+    else
+    {
+        Console.WriteLine("число '" +number3 +"' максимальное");
+    }
+} 
+else
+{   
+    if (number2 >= number3)
+    {
+        Console.WriteLine("число '" +number2 +"' максимальное");
+    }
+    else
+    {
+        Console.WriteLine("число '" +number3 +"' максимальное");
+    }
+}
