411bf51158170826b8d1ded7ba833aba4a59186a
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-10 22:19:00 +0300

    Выполнил 6 задание

--- Examples/Lesson1_home_work/Task6/Program.cs
+++ Examples/Lesson1_home_work/Task6/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 > 0)
+{
+    Console.WriteLine("число '" +number1 +"' нечётное");
+} 
+else
+{   
+    Console.WriteLine("число '" +number1 +"' чётное");
+}
