9ff24e45f234c6ca04361a5ba663112aa9f36ea5
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-10 22:28:12 +0300

    Выполнил 8 задание

--- Examples/Lesson1_home_work/Task8/Program.cs
+++ Examples/Lesson1_home_work/Task8/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+for (int i = 2; i <= number1; i = i + 2)
+{
+    Console.Write(i +",");
+}
+    
