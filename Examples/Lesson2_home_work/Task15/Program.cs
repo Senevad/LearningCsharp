50d05f7412e01ee4619e1537779656b03bcc94c4
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-15 02:32:20 +0300

    Выполнил 15 задание

--- Examples/Lesson2_home_work/Task15/Program.cs
+++ Examples/Lesson2_home_work/Task15/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите день недели");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 > 0 && number1 < 8 )
+{   
+    if (number1 >= 6) Console.WriteLine("да это выходной день");
+    else Console.WriteLine("нет это рабочий день");
+} 
+else Console.WriteLine("это не день недели");
