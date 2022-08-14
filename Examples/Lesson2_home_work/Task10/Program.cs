61b30321aabc098be7158fad8a72fe68b1f76542
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-15 01:49:19 +0300

    Выполнил 10 задание

--- Examples/Lesson2_home_work/Task10/Program.cs
+++ Examples/Lesson2_home_work/Task10/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int number = new Random().Next(100,1000);
+int number2 = number / 10 % 10 ;
+Console.WriteLine("вторая цифра числа " +number +" это " +number2);
