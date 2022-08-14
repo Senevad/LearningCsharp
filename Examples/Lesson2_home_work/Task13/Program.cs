69d5bbbae97bddbe1cbb0237773d9841d3e6224e
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-15 02:16:50 +0300

    Выполнил 13 задание

--- Examples/Lesson2_home_work/Task13/Program.cs
+++ Examples/Lesson2_home_work/Task13/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+string strnumber = Console.ReadLine();
+if (strnumber.Length > 2)
+{
+    int number = Convert.ToInt32(strnumber);
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    number = number % 10 ;
+    Console.WriteLine($"третья цифра {number}");
+} 
+else
+{   
+    Console.WriteLine("третьей цифры нет");
+}
