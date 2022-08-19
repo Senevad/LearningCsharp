767e4f530d272603a17c6a3129314850ecf280ef
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-20 02:08:44 +0300

    Выполнил 27 задание

--- Examples/Lesson4_home_work/Task27/Program.cs
+++ Examples/Lesson4_home_work/Task27/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (number % 10 != 0)
+{
+    sum = sum + (number % 10);
+    number = number / 10;
+}
+Console.WriteLine(Math.Abs(sum));
