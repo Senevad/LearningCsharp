38d0719bcf999cf0a2642b3b198c3d4d40f8b3e2
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-19 03:25:56 +0300

    Выполнил 19 задание

--- Examples/Lesson3_home_work/Task19/Program.cs
+++ Examples/Lesson3_home_work/Task19/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+if (number.Length != 5) Console.WriteLine("Число не пятизначное");
+else
+{
+    if (number[0] == number[4] && number[1] == number[3]) Console.WriteLine("да");
+    else Console.WriteLine("нет");
+}
+    
