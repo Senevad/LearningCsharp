9413eb9807e6ced7d56bfbe38c2a27a516041675
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-19 03:52:21 +0300

    Выполнил 21 задание

--- Examples/Lesson3_home_work/Task21/Program.cs
+++ Examples/Lesson3_home_work/Task21/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double length = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+Console.WriteLine($"{length:f2}");
