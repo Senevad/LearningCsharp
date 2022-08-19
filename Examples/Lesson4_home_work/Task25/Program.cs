3460e8cc94cbdfb3dd1a2a20e819e4fc21b8029f
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-20 01:58:07 +0300

    Выполнил 25 задание

--- Examples/Lesson4_home_work/Task25/Program.cs
+++ Examples/Lesson4_home_work/Task25/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+int AConst = A;
+for (int i = 1; i < B; i++)
+{
+    A = A * AConst;
+}
+Console.WriteLine(A);
