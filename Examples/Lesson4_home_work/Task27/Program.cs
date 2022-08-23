7b7efbe3c48b38d03d40310487fa992191e7b273
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-24 02:09:33 +0300

    исправил 27 задание

--- Examples/Lesson4_home_work/Task27/Program.cs
+++ Examples/Lesson4_home_work/Task27/Program.cs
@@ -2,7 +2,7 @@
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
-while (number % 10 != 0)
+while (number % 10 !=0 || number / 10 !=0)
 {
     sum = sum + (number % 10);
     number = number / 10;
