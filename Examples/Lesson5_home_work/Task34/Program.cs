60107953c6a9559bb871278b836c3d023f3dbea1
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-24 03:38:56 +0300

    выполнил 34 задание

--- Examples/Lesson5_home_work/Task34/Program.cs
+++ Examples/Lesson5_home_work/Task34/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int [] array = new int [10];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    if (array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"В массиве [{String.Join(",",array)}] - {count} четных чисел");
