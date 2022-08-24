b0e282d83a3c2bad8cf2aae7b8ceca5e64261d88
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-24 03:53:18 +0300

    выполнил 36 задание

--- Examples/Lesson5_home_work/Task36/Program.cs
+++ Examples/Lesson5_home_work/Task36/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int [] array = new int [10];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-9,10);
+    if (i % 2 != 0)
+    {
+        count += array[i];
+    }
+}
+Console.WriteLine($"В массиве [{String.Join(",",array)}] - {count} Сумма элементов стоящих на нечётных позициях");
