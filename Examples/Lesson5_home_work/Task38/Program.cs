bbd02708f86b342a8dc2efa99895244eff85dac4
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-24 04:37:09 +0300

    выполнил 38 задание

--- Examples/Lesson5_home_work/Task38/Program.cs
+++ Examples/Lesson5_home_work/Task38/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int [] array = new int [10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-99,100);
+}
+int min = array[0];
+int max = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+}
+int result = max - min;
+Console.WriteLine($"В массиве [{String.Join(",",array)}] - {result} разница между максимальным и минимальным элементом массива");
