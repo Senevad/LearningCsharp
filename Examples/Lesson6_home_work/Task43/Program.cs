2a07ef7e4eed1cad5ecb357f68fcb91fb5765c1a
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-29 02:06:14 +0300

    выполнил 43 задание

--- Examples/Lesson6_home_work/Task43/Program.cs
+++ Examples/Lesson6_home_work/Task43/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+if (k1 == k2 && b1 == b2)
+{
+    Console.WriteLine("прямые совпадают");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("прямые паралельны");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"точка пересечения ({x},{y})");
+}
