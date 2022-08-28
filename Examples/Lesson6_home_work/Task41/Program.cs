801c48672ada977594335b594424d42dc9dbcee2
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-29 01:34:33 +0300

    выполнил 41 задание

--- Examples/Lesson6_home_work/Task41/Program.cs
+++ Examples/Lesson6_home_work/Task41/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int count = 0;
+Console.WriteLine("Введите число M");
+int M = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < M; i++)
+{
+    Console.WriteLine("Введите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Пользователь ввёл {count} чисел больше 0");
