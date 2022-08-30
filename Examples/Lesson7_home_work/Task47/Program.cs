5ad9d92c32095e9e688f6be911c228c256a0c88d
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-30 20:18:09 +0300

    поправил 47 задание

--- Examples/Lesson7_home_work/Task47/Program.cs
+++ Examples/Lesson7_home_work/Task47/Program.cs
@@ -3,11 +3,11 @@ Console.WriteLine("Введите число строк");
 int row = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число столбцов");
 int column = int.Parse(Console.ReadLine());
+double[,] array = new double[row,column];
 for (int i = 0; i < row; i++)
 {
     for (int j = 0; j < column; j++)
     {
-        double[,] array = new double[row,column];
         array[i,j] = Math.Round(new Random().NextDouble(),1) + new Random().Next(-9,10);
         Console.Write(String.Format($"{array[i,j],6}"));
     }
