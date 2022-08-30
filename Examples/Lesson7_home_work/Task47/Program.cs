3b4a10b86ba94a18d9a8cd8248c49f746011a224
Senchenko_Vadim <Senevad@yandex.ru>
2022-08-30 19:54:57 +0300

    выполнил 47 задание

--- Examples/Lesson7_home_work/Task47/Program.cs
+++ Examples/Lesson7_home_work/Task47/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число строк");
+int row = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число столбцов");
+int column = int.Parse(Console.ReadLine());
+for (int i = 0; i < row; i++)
+{
+    for (int j = 0; j < column; j++)
+    {
+        double[,] array = new double[row,column];
+        array[i,j] = Math.Round(new Random().NextDouble(),1) + new Random().Next(-9,10);
+        Console.Write(String.Format($"{array[i,j],6}"));
+    }
+    Console.WriteLine();
+}
