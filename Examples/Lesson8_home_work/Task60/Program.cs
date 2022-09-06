c8d59af30f0a38b9b74ade623fa066e04f55095f
Senchenko_Vadim <Senevad@yandex.ru>
2022-09-06 05:54:15 +0300

    выполнил 60 задание

--- Examples/Lesson8_home_work/Task60/Program.cs
+++ Examples/Lesson8_home_work/Task60/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+    int[,,] array = new int[3,3,3];
+    int number = 10;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = number;
+                number++;
+            } 
+        }
+    }
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(String.Format($"{array[i, j, k],6} ({i},{j},{k})"));
+            } 
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
