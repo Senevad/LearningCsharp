0c13301f1f31a3a80b22c1747c17b9cd2eed256a
Senchenko_Vadim <Senevad@yandex.ru>
2022-09-09 06:10:42 +0300

    выполнил 68 задание

--- Examples/Lesson9_home_work/Task68/Program.cs
+++ Examples/Lesson9_home_work/Task68/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("введите М");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("введите N");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine(FunctionAcr(M, N));
+
+int FunctionAcr(int numM, int numN)
+{
+    if (numM == 0) return (numN + 1);
+    else if (numM != 0 && numN == 0) return FunctionAcr(numM - 1, 1);
+    else return FunctionAcr(numM - 1, FunctionAcr(numM, numN - 1));
+}
