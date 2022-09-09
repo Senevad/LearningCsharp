5ec176427d41d0f31ee6abdc50162d0a666ee4f4
Senchenko_Vadim <Senevad@yandex.ru>
2022-09-09 05:33:44 +0300

    выполнил 66 задание

--- Examples/Lesson9_home_work/Task66/Program.cs
+++ Examples/Lesson9_home_work/Task66/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("введите М");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("введите N");
+int N = int.Parse(Console.ReadLine());
+
+void PrintNumbers(int start, int end, int sum)
+{
+    sum += start;
+    if (start == end)
+    {
+        Console.Write($"{sum} ");
+        return;
+    }
+    PrintNumbers(start + 1, end, sum);
+}
+
+PrintNumbers(M, N, 0);
